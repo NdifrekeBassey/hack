dc898cff2539a4d8709daac76acd98c52686b728
Ndifreke Bassey <ndifrekebassey13@gmail.com>
2023-08-22 23:16:52 +0100

    Just having fun with javascript

---
+++ hack.js
@@ -0,0 +1,27 @@
+alert("Don't be scared.Its just a little project!.thank the coder😁")
+$(function () {
+    $("#hide").hide();
+    $(".back").hide();
+})
+function show() {
+    $(function () {
+        $("#hide").show();
+        $(".button").hide();
+        navigator.vibrate(80000, 100000);
+        $(".back").show();
+        alert("☠️HaHaHaHa !! So, finally you clicked the button☠️");
+        alert("Gathering data.... !\n Done☠️");
+        alert("Hack your mobile.... !\n Done☠️");
+        alert("Getting all information....! \n Done☠️");
+        console.log("☠️HaHaHaHa !! So, finally you clicked the button☠️\n\n");
+        console.log("Gathering data.... !\n Done☠️\n");
+        console.log("Hack your mobile.... !\n Done☠️\n");
+        console.log("Getting all information....! \n Done☠️\n");
+        for (let i = 0; i < 5; ++i) {
+
+            console.log("☠️Hacked☠️☠️Hacked☠️\n");
+
+        }
+        console.log("Are you scared or not. Tell me in the comment section!😁");
+    })
+}
